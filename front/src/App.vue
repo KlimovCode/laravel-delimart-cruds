1cc44ce8750deb49d4c5bc61c90017de0cb70583
Klimov Code <klimovcode@mail.ru>
2021-05-18 20:32:21 +0500

    init front

---
+++ front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue!</h1>
+    <h2>{{ test }}</h2>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+  export default {
+    data: () => ({
+      test: 'test'
+    })
+  }
+</script>
