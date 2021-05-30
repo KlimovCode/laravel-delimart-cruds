259e3d23f5130466879529f7c8706a56f0823745
Klimov Code <klimovcode@mail.ru>
2021-05-30 18:14:19 +0500

    work with api

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -27,7 +27,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: "/front/dist",
+  // base: "/front/dist",
   routes,
 });
 
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,3 +1,44 @@
 <template>
-    <h1>Hello Vue Router!</h1>
+  <div class="wrapper">
+      <h1>Cruds</h1>
+      <Crud />
+      <Crud />
+      <Crud />
+  </div>
 </template>
+
+<script>
+import Crud from '../components/Crud.vue'
+import axiosBase from 'axios'
+const axios = axiosBase.create({
+  baseURL: 'http://api.laravel-demiart-cruds/back/public/'
+});
+
+function Crud({ id, color, name}) {
+    this.id = id;
+    this.color = color;
+    this.name = name;
+}
+
+export default {
+    components: { Crud },
+    data() {
+      return {
+        cruds: []
+      }
+    },
+    methods: {
+        async read() {
+            const data = axios.get('api/cruds')
+            console.log(data);
+        }
+    },
+    created() {
+        this.read()
+    }
+}
+</script>
+
+<style>
+
+</style>
