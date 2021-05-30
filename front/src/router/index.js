927387c6c472a937743e778411887c6171a43ecd
Klimov Code <klimovcode@mail.ru>
2021-05-30 08:57:52 +0500

    add 404 component, update router

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Test from "../views/Test.vue";
+import NotFound from "../views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -17,11 +18,16 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "Test" */ "../views/Test.vue"),
   },
+  {
+    path: "*",
+    name: "404",
+    component: NotFound,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: "/",
+  base: "/front/dist",
   routes,
 });
 
---
+++ front/src/views/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Not found page ([ERROR]: 404)</h1>
+</template>
