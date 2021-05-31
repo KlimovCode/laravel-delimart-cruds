7830d648c2a4acdbde16c874350982b61fab0727
Klimov Code <klimovcode@mail.ru>
2021-05-31 07:46:43 +0500

    add mute UX, fix delete function, delete default nav

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="wrapper">
       <h1>Cruds</h1>
-      <CrudComponent
-        v-for="crud in cruds"
-        v-bind="crud"
-        :key="crud.id"
-        @update="update"
-        @delete="del"
-      ></CrudComponent>
+      <div class="cruds__wrapper">
+        <CrudComponent
+            v-for="crud in cruds"
+            v-bind="crud"
+            :key="crud.id"
+            @update="update"
+            @delete="del"
+        ></CrudComponent>
+      </div>
       <div>
          <button @click="create">Add</button>
       </div>
@@ -31,35 +33,53 @@ export default {
     components: { CrudComponent },
     data() {
       return {
-        cruds: []
+        cruds: [],
+        mute: false
       }
     },
     methods: {
         async read() {
+            this.mute = true
             const { data } = await axios.get('api/cruds')
             data.forEach(crud => this.cruds.push(new Crud(crud)))
+            this.mute = false
         },
         async update(id, color) {
+            this.mute = true
             await axios.put(`/api/cruds/${id}`, { color })
             this.cruds.find(crud => crud.id === id).color = color
+            this.mute = false
         },
         async del(id) {
-            await axios.get('api/cruds')
+            this.mute = true
+            await axios.delete(`api/cruds/${id}`)
             let index = this.cruds.findIndex(crud => crud.id === id)
             this.cruds.splice(index, 1)
+            this.mute = false
         },
         async create() {
+            this.mute = true
             const { data } = await axios.get('api/cruds/create')
             console.log(data);
             this.cruds.push(new Crud(data))
+            this.mute = false
         }
     },
     created() {
         this.read()
-    }
+    },
+    watch: {
+      mute(val) {
+        document.getElementById('mute').className = val ? "on" : "";
+      }
+    },
 }
 </script>
 
 <style>
-
+    .cruds__wrapper {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 1em 2em;
+    }
 </style>
